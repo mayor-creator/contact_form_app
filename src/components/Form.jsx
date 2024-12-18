09e071a0f4589c3f84ac6a45d15652036eb35782
Mayor Creator <paulaboagye@outlook.com>
2024-12-17 21:27:01 -0500

    add state data to manage the change of data

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,4 +1,13 @@
+import { useState } from "react";
+
 export function Form() {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [isSelected, setIsSelected] = useState("");
+  const [message, setMessage] = useState("");
+  const [checkConsent, setCheckConsent] = useState(false);
+
   return (
     <>
       <form>
@@ -12,6 +21,10 @@ export function Form() {
                 id="firstName"
                 required={true}
                 maxLength={20}
+                value={firstName}
+                onChange={(event) => {
+                  setFirstName(event.target.value);
+                }}
               />
             </label>
           </div>
@@ -24,6 +37,10 @@ export function Form() {
                 id="lastName"
                 required={true}
                 maxLength={20}
+                value={lastName}
+                onChange={(event) => {
+                  setLastName(event.target.value);
+                }}
               />
             </label>
           </div>
@@ -36,6 +53,8 @@ export function Form() {
               name="emailAddress"
               id="emailAddress"
               required={true}
+              value={email}
+              onChange={(event) => setEmail(event.target.value)}
             />
           </label>
         </div>
@@ -45,13 +64,29 @@ export function Form() {
           </legend>
           <div className="general_enquiry_container">
             <label htmlFor="generalEnquiryRadio">
-              <input type="radio" name="query_type" id="generalEnquiry" />{" "}
+              <input
+                type="radio"
+                name="query_type"
+                id="generalEnquiry"
+                required={true}
+                value="general-enquiry"
+                checked={isSelected === "general-enquiry"}
+                onChange={(event) => setIsSelected(event.target.value)}
+              />{" "}
               General Enquiry
             </label>
           </div>
           <div className="support_request_container">
             <label htmlFor="supportRequestRadio">
-              <input type="radio" name="query_type" id="supportRequest" />{" "}
+              <input
+                type="radio"
+                name="query_type"
+                id="supportRequest"
+                required={true}
+                value="support-request"
+                checked={isSelected === "support-request"}
+                onChange={(event) => setIsSelected(event.target.value)}
+              />{" "}
               Support Request
             </label>
           </div>
@@ -59,17 +94,36 @@ export function Form() {
         <div className="message_container">
           <label htmlFor="message">
             Message <span>*</span>
-            <textarea name="message" id="message" required={true}></textarea>
+            <textarea
+              name="message"
+              id="message"
+              required={true}
+              value={message}
+              onChange={(event) => setMessage(event.target.value)}
+            ></textarea>
           </label>
         </div>
         <div className="consent_container">
-          <input type="checkbox" name="consent" id="consent" required={true} />
+          <input
+            type="checkbox"
+            name="consent"
+            id="consent"
+            required={true}
+            checked={checkConsent}
+            onChange={(event) => setCheckConsent(event.target.checked)}
+          />
           <label htmlFor="consent">
             I consent to being contacted by the team <span>*</span>
           </label>
         </div>
         <button type="submit">Submit</button>
       </form>
+      <p>First Name {firstName}</p>
+      <p>Last Name {lastName}</p>
+      <p>Email {email}</p>
+      <p>Radio Button Selected {isSelected}</p>
+      <p>Message {message}</p>
+      <p>Consent {checkConsent.toString()}</p>
     </>
   );
 }
