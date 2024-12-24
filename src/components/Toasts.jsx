b0d166dc5bc7c8a3db9135351cda9ae924fcbb4d
Mayor Creator <paulaboagye@outlook.com>
2024-12-24 02:32:51 -0500

    add: useEffect and UseState hook to manage the toasts notification

--- src/components/Toasts.jsx
+++ src/components/Toasts.jsx
@@ -1,11 +1,22 @@
+import { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 
 import checkIcon from "../assets/images/icon-success-check.svg";
 import styles from "./Toasts.module.css";
 
 export function Toasts({ message }) {
+  const [show, setShow] = useState(true);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setShow(false);
+    }, 3000);
+
+    return () => clearTimeout(timeout);
+  }, []);
+
   return (
-    <div className={styles.container}>
+    <div className={`${styles.container} ${show ? ` ${styles.show}` : ""}`}>
       <div className={styles.top_container}>
         <img src={checkIcon} alt="" />
         <p className={styles.title}>Message Sent!</p>
